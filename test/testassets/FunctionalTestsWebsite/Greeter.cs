f65bda863daa82eb52ae592cd9c1790bcc57b683
James Newton-King <james@newtonking.com>
2019-01-23 13:27:57 +1300

    Add functional test website and tests (#6)

--- test/testassets/FunctionalTestsWebsite/Greeter.cs
+++ test/testassets/FunctionalTestsWebsite/Greeter.cs
@@ -1,3 +1,20 @@
+#region Copyright notice and license
+
+// Copyright 2015 gRPC authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#endregion
 
 using System.Threading.Tasks;
 using Grpc.Core;
@@ -22,14 +39,18 @@ class GreeterService : Greeter.GreeterBase
 
     public override async Task SayHellos(HelloRequest request, IServerStreamWriter<HelloReply> responseStream, ServerCallContext context)
     {
-        for (int i = 0; i < 3; i++)
+        for (var i = 0; i < 3; i++)
         {
+            // Gotta look busy
+            await Task.Delay(100);
+
             var message = $"How are you {request.Name}? {i}";
             _logger.LogInformation($"Sending greeting {message}");
             await responseStream.WriteAsync(new HelloReply { Message = message });
-            // Gotta look busy
-            await Task.Delay(1000);
         }
+
+        // Gotta look busy
+        await Task.Delay(100);
 
         _logger.LogInformation("Sending goodbye");
         await responseStream.WriteAsync(new HelloReply { Message = $"Goodbye {request.Name}!" });
