a65f916c839e2b6d9458722ec51f69a4d9121c9a
James Newton-King <james@newtonking.com>
2019-04-07 07:47:52 +1200

    React to latest .NET Core SDK

--- testassets/BenchmarkWorkerWebsite/ServerRunners.cs
+++ testassets/BenchmarkWorkerWebsite/ServerRunners.cs
@@ -118,9 +118,10 @@ namespace BenchmarkWorkerWebsite
             // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
             public void Configure(IApplicationBuilder app)
             {
-                app.UseRouting(builder =>
+                app.UseRouting();
+                app.UseEndpoints(endpoints =>
                 {
-                    builder.MapGrpcService<BenchmarkServiceImpl>();
+                    endpoints.MapGrpcService<BenchmarkServiceImpl>();
                 });
             }
         }
