b8b4879b55742026bb829383943125a768ec6acd
James Newton-King <james@newtonking.com>
2019-05-30 06:55:38 +1200

    Add special_status_message interop test (#272)

--- testassets/InteropTestsClient/InteropClient.cs
+++ testassets/InteropTestsClient/InteropClient.cs
@@ -271,6 +271,9 @@ namespace InteropTestsClient
                 case "unimplemented_service":
                     RunUnimplementedService(CreateClient<UnimplementedService.UnimplementedServiceClient>(channel));
                     break;
+                case "special_status_message":
+                    await RunSpecialStatusMessageAsync(client);
+                    break;
                 case "unimplemented_method":
                     RunUnimplementedMethod(client);
                     break;
@@ -648,6 +651,33 @@ namespace InteropTestsClient
                     Assert.AreEqual(StatusCode.Unknown, e.Status.StatusCode);
                     Assert.AreEqual(echoStatus.Message, e.Status.Detail);
                 }
+            }
+
+            Console.WriteLine("Passed!");
+        }
+
+        private static async Task RunSpecialStatusMessageAsync(TestService.TestServiceClient client)
+        {
+            Console.WriteLine("running special_status_message");
+
+            var echoStatus = new EchoStatus
+            {
+                Code = 2,
+                Message = "\t\ntest with whitespace\r\nand Unicode BMP ☺ and non-BMP 😈\t\n"
+            };
+
+            try
+            {
+                await client.UnaryCallAsync(new SimpleRequest
+                {
+                    ResponseStatus = echoStatus
+                });
+                Assert.Fail();
+            }
+            catch (RpcException e)
+            {
+                Assert.AreEqual(StatusCode.Unknown, e.Status.StatusCode);
+                Assert.AreEqual(echoStatus.Message, e.Status.Detail);
             }
 
             Console.WriteLine("Passed!");
