a0f0c915e5ddc351b54f396f7fab475973ac899c
James Newton-King <james@newtonking.com>
2019-05-31 14:32:06 +1200

    Add functional test using Any (#279)

---
+++ testassets/FunctionalTestsWebsite/Services/AnyService.cs
@@ -0,0 +1,55 @@
+#region Copyright notice and license
+
+// Copyright 2019 The gRPC Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#endregion
+
+using System;
+using System.Threading.Tasks;
+using Any;
+using Grpc.Core;
+using AnyMessage = Google.Protobuf.WellKnownTypes.Any;
+
+namespace FunctionalTestsWebsite.Services
+{
+    public class AnyService : Any.AnyService.AnyServiceBase
+    {
+        public override Task<AnyMessageResponse> DoAny(AnyMessage request, ServerCallContext context)
+        {
+            AnyMessageResponse anyMessageResponse;
+            switch (request.TypeUrl)
+            {
+                case "type.googleapis.com/Any.AnyProductRequest":
+                    var product = request.Unpack<AnyProductRequest>();
+                    anyMessageResponse = new AnyMessageResponse
+                    {
+                        Message = $"{product.Quantity} x {product.Name}"
+                    };
+                    break;
+                case "type.googleapis.com/Any.AnyUserRequest":
+                    var user = request.Unpack<AnyUserRequest>();
+                    anyMessageResponse = new AnyMessageResponse
+                    {
+                        Message = $"{user.Name} - {(user.Enabled ? "Enabled" : "Disabled")}"
+                    };
+                    break;
+                default:
+                    throw new InvalidOperationException("Unexpected type URL.");
+            }
+
+            return Task.FromResult(anyMessageResponse);
+        }
+    }
+}
