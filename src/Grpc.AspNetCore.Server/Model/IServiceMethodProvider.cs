382e5ef6e268300ed056e9a2732ad58adbf04818
James Newton-King <james@newtonking.com>
2019-06-04 16:52:48 +1200

    Add XML docs to method provider APIs (#289)

--- src/Grpc.AspNetCore.Server/Model/IServiceMethodProvider.cs
+++ src/Grpc.AspNetCore.Server/Model/IServiceMethodProvider.cs
@@ -19,14 +19,21 @@
 namespace Grpc.AspNetCore.Server.Model
 {
     /// <summary>
-    /// 
+    /// Defines a contract for specifying methods for <typeparamref name="TService"/>.
     /// </summary>
+    /// <remarks>
+    /// <para>
+    /// On application initialization, gRPC invokes all registered instances of <see cref="IServiceMethodProvider{TService}"/> to 
+    /// perform method discovery. 
+    /// <see cref="IServiceMethodProvider{TService}"/> instances are invoked in the order they are registered.
+    /// </para>
+    /// </remarks>
     public interface IServiceMethodProvider<TService> where TService : class
     {
         /// <summary>
-        /// 
+        /// Called to execute the provider.
         /// </summary>
-        /// <param name="context"></param>
+        /// <param name="context">The <see cref="ServiceMethodProviderContext{TService}"/>.</param>
         void OnServiceMethodDiscovery(ServiceMethodProviderContext<TService> context);
     }
 }
