8997b0d89d6bce71a65c47809345f62468f9cdda
John Luo <johluo@microsoft.com>
2019-07-29 02:31:33 -0700

    Add List command

---
+++ src/dotnet-grpc/Commands/ListCommand.cs
@@ -0,0 +1,83 @@
+﻿#region Copyright notice and license
+
+// Copyright 2019 The gRPC Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#endregion
+
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO;
+using System.Linq;
+using Grpc.Dotnet.Cli.Internal;
+using Grpc.Dotnet.Cli.Options;
+using Grpc.Dotnet.Cli.Properties;
+using Microsoft.Build.Evaluation;
+
+namespace Grpc.Dotnet.Cli.Commands
+{
+    internal class ListCommand : CommandBase
+    {
+        public ListCommand(IConsole console, FileInfo? projectPath)
+            : base(console, projectPath) { }
+
+        public static Command Create()
+        {
+            var command = new Command(
+                name: "list",
+                description: CoreStrings.RefreshCommandDescription);
+
+            command.AddOption(CommonOptions.ProjectOption());
+
+            command.Handler = CommandHandler.Create<IConsole, FileInfo>(
+                (console, project) =>
+                {
+                    try
+                    {
+                        var command = new ListCommand(console, project);
+                        command.List();
+
+                        return 0;
+                    }
+                    catch (CLIToolException e)
+                    {
+                        console.LogError(e);
+
+                        return -1;
+                    }
+                });
+
+            return command;
+        }
+
+        public  void List()
+        {
+            // TODO Resx
+            Console.Log("Protobuf references:");
+            Console.Log("");
+
+            foreach (var reference in Project.GetItems(ProtobufElement))
+            {
+                if (reference.HasMetadata(SourceUrlElement))
+                {
+                    Console.Log($"URL reference: {reference.UnevaluatedInclude} from {reference.GetMetadataValue(SourceUrlElement)}");
+                }
+                else
+                {
+                    Console.Log($"File reference: {reference.UnevaluatedInclude}");
+                }
+            }
+        }
+    }
+}
---
+++ test/dotnet-grpc.Tests/ListCommandTests.cs
@@ -0,0 +1,57 @@
+﻿#region Copyright notice and license
+
+// Copyright 2019 The gRPC Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#endregion
+
+using System;
+using System.CommandLine;
+using System.IO;
+using System.Threading.Tasks;
+using Grpc.Dotnet.Cli.Commands;
+using Grpc.Dotnet.Cli.Properties;
+using NUnit.Framework;
+
+namespace Grpc.Dotnet.Cli.Tests
+{
+    [TestFixture]
+    public class ListCommandTests : TestBase
+    {
+        [NonParallelizable]
+        [Test]
+        public void List_ListsReferences()
+        {
+            // Arrange
+            var currentDir = Directory.GetCurrentDirectory();
+            var tempDir = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+            var testConsole = new TestConsole();
+            new DirectoryInfo(Path.Combine(currentDir, "TestAssets", "MultipleReferences")).CopyTo(tempDir);
+
+            // Act
+            Directory.SetCurrentDirectory(tempDir);
+            var command = new ListCommand(testConsole, null);
+            command.List();
+
+            // Assert
+            var output = testConsole.Out.ToString()!;
+            Assert.True(output.Contains("URL reference: Proto/a.proto from https://contoso.com/greet.proto"));
+            Assert.True(output.Contains("File reference: Proto/b.proto"));
+
+            // Cleanup
+            Directory.SetCurrentDirectory(currentDir);
+            Directory.Delete(tempDir, true);
+        }
+    }
+}
