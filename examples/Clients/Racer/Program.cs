89493e3421f1783c867d0663779fc8e2bec12a8a
James Newton-King <james@newtonking.com>
2019-08-12 17:18:49 +1200

    Add new bi-directional streaming sample (#440)

---
+++ examples/Clients/Racer/Program.cs
@@ -0,0 +1,84 @@
+﻿#region Copyright notice and license
+
+// Copyright 2019 The gRPC Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#endregion
+
+using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Net.Client;
+using Race;
+
+namespace Sample.Clients
+{
+    class Program
+    {
+        private static readonly TimeSpan RaceDuration = TimeSpan.FromSeconds(30);
+
+        static async Task Main(string[] args)
+        {
+            var httpClient = new HttpClient { BaseAddress = new Uri($"http://localhost:50051") };
+            var channelBuilder = ChannelBuilder.ForHttpClient(httpClient);
+            var client = new Racer.RacerClient(channelBuilder.Build());
+
+            Console.WriteLine("Press any key to start race...");
+            Console.ReadKey();
+
+            await BidirectionalStreamingExample(client);
+
+            Console.WriteLine("Shutting down");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        private static async Task BidirectionalStreamingExample(Racer.RacerClient client)
+        {
+            var headers = new Metadata { new Metadata.Entry("race-duration", RaceDuration.ToString()) };
+
+            Console.WriteLine("Ready, set, go!");
+            using (var call = client.ReadySetGo(new CallOptions(headers)))
+            {
+
+                // Read incoming messages in a background task
+                RaceMessage? lastMessageReceived = null;
+                var readTask = Task.Run(async () =>
+                {
+                    await foreach (var message in call.ResponseStream.ReadAllAsync())
+                    {
+                        lastMessageReceived = message;
+                    }
+                });
+
+                // Write outgoing messages until timer is complete
+                var sw = Stopwatch.StartNew();
+                var sent = 0;
+                while (sw.Elapsed < RaceDuration)
+                {
+                    await call.RequestStream.WriteAsync(new RaceMessage { Count = ++sent });
+                }
+
+                // Finish call and report results
+                await call.RequestStream.CompleteAsync();
+                await readTask;
+
+                Console.WriteLine($"Messages sent: {sent}");
+                Console.WriteLine($"Messages received: {lastMessageReceived?.Count ?? 0}");
+            }
+        }
+    }
+}
---
+++ testassets/FunctionalTestsWebsite/Services/RacerService.cs
@@ -0,0 +1,55 @@
+﻿#region Copyright notice and license
+
+// Copyright 2019 The gRPC Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#endregion
+
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Race;
+
+namespace FunctionalTestsWebsite.Services
+{
+    public class RacerService : Racer.RacerBase
+    {
+        public override async Task ReadySetGo(IAsyncStreamReader<RaceMessage> requestStream, IServerStreamWriter<RaceMessage> responseStream, ServerCallContext context)
+        {
+            var raceDuration = TimeSpan.Parse(context.RequestHeaders.Single(h => h.Key == "race-duration").Value);
+
+            // Read incoming messages in a background task
+            RaceMessage? lastMessageReceived = null;
+            var readTask = Task.Run(async () =>
+            {
+                while (await requestStream.MoveNext())
+                {
+                    lastMessageReceived = requestStream.Current;
+                }
+            });
+
+            // Write outgoing messages until timer is complete
+            var sw = Stopwatch.StartNew();
+            var sent = 0;
+            while (sw.Elapsed < raceDuration)
+            {
+                await responseStream.WriteAsync(new RaceMessage { Count = ++sent });
+            }
+
+            await readTask;
+        }
+    }
+}
