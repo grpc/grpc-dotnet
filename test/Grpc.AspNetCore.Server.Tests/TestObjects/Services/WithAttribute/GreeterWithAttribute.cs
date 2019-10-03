a1422a5e15768f7c639ba0309ddaf35af740c4ce
James Newton-King <james@newtonking.com>
2019-10-04 10:57:47 +1300

    Change example and test package names to lowercase

--- test/Grpc.AspNetCore.Server.Tests/TestObjects/Services/WithAttribute/GreeterWithAttribute.cs
+++ test/Grpc.AspNetCore.Server.Tests/TestObjects/Services/WithAttribute/GreeterWithAttribute.cs
@@ -23,7 +23,7 @@ namespace Grpc.AspNetCore.Server.Tests.TestObjects.Services.WithAttribute
 {
     public static partial class GreeterWithAttribute
     {
-        static readonly string __ServiceName = "Greet.Greeter";
+        static readonly string __ServiceName = "greet.Greeter";
 
         static readonly Marshaller<global::Greet.HelloRequest> __Marshaller_Greet_HelloRequest = Marshallers.Create((arg) => global::Google.Protobuf.MessageExtensions.ToByteArray(arg), global::Greet.HelloRequest.Parser.ParseFrom);
         static readonly Marshaller<global::Greet.HelloReply> __Marshaller_Greet_HelloReply = Marshallers.Create((arg) => global::Google.Protobuf.MessageExtensions.ToByteArray(arg), global::Greet.HelloReply.Parser.ParseFrom);
