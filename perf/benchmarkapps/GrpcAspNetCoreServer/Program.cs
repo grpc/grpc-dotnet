844c6a7162300141591e9c031a4f3f210e2d1ca9
James Newton-King <james@newtonking.com>
2019-10-04 13:56:59 +1300

    Clean up

--- perf/benchmarkapps/GrpcAspNetCoreServer/Program.cs
+++ perf/benchmarkapps/GrpcAspNetCoreServer/Program.cs
@@ -19,7 +19,6 @@
 using System;
 using System.IO;
 using System.Reflection;
-using System.Threading;
 using Common;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -30,18 +29,6 @@ using Microsoft.Extensions.Logging;
 
 namespace GrpcAspNetCoreServer
 {
-    public class sdfsf : IHostApplicationLifetime
-    {
-        public CancellationToken ApplicationStarted { get; }
-        public CancellationToken ApplicationStopped { get; }
-        public CancellationToken ApplicationStopping { get; }
-
-        public void StopApplication()
-        {
-            throw new NotImplementedException();
-        }
-    }
-
     public class Program
     {
         public static void Main(string[] args)
@@ -99,10 +86,6 @@ namespace GrpcAspNetCoreServer
                             }
                         });
                     });
-                })
-                .ConfigureServices(services =>
-                {
-
                 })
                 .ConfigureLogging(loggerFactory =>
                 {
