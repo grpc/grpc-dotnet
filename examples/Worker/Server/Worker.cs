e1ffb6c0c5cafd347e66de2e551da1ea8386c5fc
James Newton-King <james@newtonking.com>
2019-10-30 15:07:38 +1300

    Update server to use worker template

--- examples/Worker/Server/Worker.cs
+++ examples/Worker/Server/Worker.cs
@@ -16,28 +16,30 @@
 
 #endregion
 
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Server
 {
-    public class Program
+    public class Worker : BackgroundService
     {
-        public static void Main(string[] args)
+        private readonly ILogger<Worker> _logger;
+
+        public Worker(ILogger<Worker> logger)
         {
-            CreateHostBuilder(args).Build().Run();
+            _logger = logger;
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                })
-                .ConfigureServices((hostContext, services) =>
-                {
-                    services.AddHostedService<Worker>();
-                });
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogDebug("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(10000, stoppingToken);
+            }
+        }
     }
 }
