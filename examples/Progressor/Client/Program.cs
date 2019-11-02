6910eb09cd76131c7a77a8b750cca36fa02f624b
James Newton-King <james@newtonking.com>
2019-11-02 16:33:56 +1300

    Add progessor example (#597)

---
+++ examples/Progressor/Client/Program.cs
@@ -0,0 +1,60 @@
+#region Copyright notice and license
+
+// Copyright 2019 The gRPC Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#endregion
+
+using System;
+using System.Threading.Tasks;
+using Client.ResponseProgress;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Grpc.Net.Client;
+using Progress;
+
+namespace Client
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Progressor.ProgressorClient(channel);
+
+            var progress = new Progress<int>(i => Console.WriteLine($"Progress: {i}%"));
+
+            var result = await ServerStreamingCallExample(client, progress);
+
+            Console.WriteLine("Preparing results...");
+            await Task.Delay(TimeSpan.FromSeconds(2));
+
+            foreach (var item in result.Items)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Shutting down");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        private static ResponseProgress<HistoryResult, int> ServerStreamingCallExample(Progressor.ProgressorClient client, IProgress<int> progress)
+        {
+            var call = client.RunHistory(new Empty());
+
+            return GrpcProgress.Create(call.ResponseStream, progress);
+        }
+    }
+}
---
+++ examples/Progressor/Client/ResponseProgress/ResponseProgress.cs
@@ -0,0 +1,73 @@
+﻿#region Copyright notice and license
+
+// Copyright 2019 The gRPC Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#endregion
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Grpc.Core;
+
+namespace Client.ResponseProgress
+{
+    public static class GrpcProgress
+    {
+        public static ResponseProgress<TResult, TProgress> Create<TResult, TProgress>(
+            IAsyncStreamReader<IProgressMessage<TResult, TProgress>> streamReader,
+            IProgress<TProgress>? progress = null)
+        {
+            return new ResponseProgress<TResult, TProgress>(streamReader, progress);
+        }
+    }
+
+    public class ResponseProgress<TResult, TProgress> : Progress<TProgress>
+    {
+        private readonly IAsyncStreamReader<IProgressMessage<TResult, TProgress>> _streamReader;
+        private readonly IProgress<TProgress>? _progress;
+        private readonly Task<TResult> _resultTask;
+
+        public ResponseProgress(IAsyncStreamReader<IProgressMessage<TResult, TProgress>> streamReader, IProgress<TProgress>? progress = null)
+        {
+            _streamReader = streamReader;
+            _progress = progress;
+
+            // Start reading from the stream in the background, updating IProgress with values from the server.
+            // When the result is returned set it into the task complete source.
+            _resultTask = Task.Run<TResult>(async () =>
+            {
+                await foreach (var item in _streamReader.ReadAllAsync())
+                {
+                    if (item.IsProgress)
+                    {
+                        _progress?.Report(item.Progress);
+                        OnReport(item.Progress);
+                    }
+                    if (item.IsResult)
+                    {
+                        return item.Result;
+                    }
+                }
+
+                throw new Exception("Call completed without a result.");
+            });
+        }
+
+        public TaskAwaiter<TResult> GetAwaiter()
+        {
+            return _resultTask.GetAwaiter();
+        }
+    }
+}
