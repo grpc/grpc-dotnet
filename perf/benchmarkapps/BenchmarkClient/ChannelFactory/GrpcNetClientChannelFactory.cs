9c6fc5120af571778cb39a0a0816a19fb2e40ae2
James Newton-King <james@newtonking.com>
2019-12-11 13:56:34 +1300

    Add client certificate option to perf benchmarks (#690)

--- perf/benchmarkapps/BenchmarkClient/ChannelFactory/GrpcNetClientChannelFactory.cs
+++ perf/benchmarkapps/BenchmarkClient/ChannelFactory/GrpcNetClientChannelFactory.cs
@@ -16,6 +16,9 @@
 
 #endregion
 
+using System.IO;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Grpc.Core;
 using Grpc.Net.Client;
@@ -25,20 +28,36 @@ namespace BenchmarkClient.ChannelFactory
     public class GrpcNetClientChannelFactory : IChannelFactory
     {
         private readonly string _target;
+        private readonly bool _useTls;
         private readonly bool _useClientCertificate;
 
-        public GrpcNetClientChannelFactory(string target, bool useClientCertificate)
+        public GrpcNetClientChannelFactory(string target, bool useTls, bool useClientCertificate)
         {
             _target = target;
+            _useTls = useTls;
             _useClientCertificate = useClientCertificate;
         }
 
         public Task<ChannelBase> CreateAsync()
         {
-            var url = _useClientCertificate ? "https://" : "http://";
+            var url = _useTls ? "https://" : "http://";
             url += _target;
 
-            var channel = GrpcChannel.ForAddress(url);
+            var httpClientHandler = new HttpClientHandler();
+            httpClientHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+
+            if (_useClientCertificate)
+            {
+                var basePath = Path.GetDirectoryName(typeof(Program).Assembly.Location);
+                var certPath = Path.Combine(basePath!, "Certs", "client.pfx");
+                var clientCertificate = new X509Certificate2(certPath, "1111");
+                httpClientHandler.ClientCertificates.Add(clientCertificate);
+            }
+
+            var channel = GrpcChannel.ForAddress(url, new GrpcChannelOptions
+            {
+                HttpClient = new HttpClient(httpClientHandler)
+            });
 
             return Task.FromResult<ChannelBase>(channel);
         }
