02cb6d12ba15a6f0a5cb4e28e7c7ab2767e3b617
James Newton-King <james@newtonking.com>
2019-12-14 16:25:12 +1300

    Add gRPC-Web Browser example

---
+++ examples/Browser/Server/wwwroot/Scripts/greet_grpc_web_pb.js
@@ -0,0 +1,225 @@
+/**
+ * @fileoverview gRPC-Web generated client stub for greet
+ * @enhanceable
+ * @public
+ */
+
+// GENERATED CODE -- DO NOT EDIT!
+
+
+
+const grpc = {};
+grpc.web = require('grpc-web');
+
+const proto = {};
+proto.greet = require('./greet_pb.js');
+
+/**
+ * @param {string} hostname
+ * @param {?Object} credentials
+ * @param {?Object} options
+ * @constructor
+ * @struct
+ * @final
+ */
+proto.greet.GreeterClient =
+    function(hostname, credentials, options) {
+  if (!options) options = {};
+  options['format'] = 'text';
+
+  /**
+   * @private @const {!grpc.web.GrpcWebClientBase} The client
+   */
+  this.client_ = new grpc.web.GrpcWebClientBase(options);
+
+  /**
+   * @private @const {string} The hostname
+   */
+  this.hostname_ = hostname;
+
+};
+
+
+/**
+ * @param {string} hostname
+ * @param {?Object} credentials
+ * @param {?Object} options
+ * @constructor
+ * @struct
+ * @final
+ */
+proto.greet.GreeterPromiseClient =
+    function(hostname, credentials, options) {
+  if (!options) options = {};
+  options['format'] = 'text';
+
+  /**
+   * @private @const {!grpc.web.GrpcWebClientBase} The client
+   */
+  this.client_ = new grpc.web.GrpcWebClientBase(options);
+
+  /**
+   * @private @const {string} The hostname
+   */
+  this.hostname_ = hostname;
+
+};
+
+
+/**
+ * @const
+ * @type {!grpc.web.MethodDescriptor<
+ *   !proto.greet.HelloRequest,
+ *   !proto.greet.HelloReply>}
+ */
+const methodDescriptor_Greeter_SayHello = new grpc.web.MethodDescriptor(
+  '/greet.Greeter/SayHello',
+  grpc.web.MethodType.UNARY,
+  proto.greet.HelloRequest,
+  proto.greet.HelloReply,
+  /**
+   * @param {!proto.greet.HelloRequest} request
+   * @return {!Uint8Array}
+   */
+  function(request) {
+    return request.serializeBinary();
+  },
+  proto.greet.HelloReply.deserializeBinary
+);
+
+
+/**
+ * @const
+ * @type {!grpc.web.AbstractClientBase.MethodInfo<
+ *   !proto.greet.HelloRequest,
+ *   !proto.greet.HelloReply>}
+ */
+const methodInfo_Greeter_SayHello = new grpc.web.AbstractClientBase.MethodInfo(
+  proto.greet.HelloReply,
+  /**
+   * @param {!proto.greet.HelloRequest} request
+   * @return {!Uint8Array}
+   */
+  function(request) {
+    return request.serializeBinary();
+  },
+  proto.greet.HelloReply.deserializeBinary
+);
+
+
+/**
+ * @param {!proto.greet.HelloRequest} request The
+ *     request proto
+ * @param {?Object<string, string>} metadata User defined
+ *     call metadata
+ * @param {function(?grpc.web.Error, ?proto.greet.HelloReply)}
+ *     callback The callback function(error, response)
+ * @return {!grpc.web.ClientReadableStream<!proto.greet.HelloReply>|undefined}
+ *     The XHR Node Readable Stream
+ */
+proto.greet.GreeterClient.prototype.sayHello =
+    function(request, metadata, callback) {
+  return this.client_.rpcCall(this.hostname_ +
+      '/greet.Greeter/SayHello',
+      request,
+      metadata || {},
+      methodDescriptor_Greeter_SayHello,
+      callback);
+};
+
+
+/**
+ * @param {!proto.greet.HelloRequest} request The
+ *     request proto
+ * @param {?Object<string, string>} metadata User defined
+ *     call metadata
+ * @return {!Promise<!proto.greet.HelloReply>}
+ *     A native promise that resolves to the response
+ */
+proto.greet.GreeterPromiseClient.prototype.sayHello =
+    function(request, metadata) {
+  return this.client_.unaryCall(this.hostname_ +
+      '/greet.Greeter/SayHello',
+      request,
+      metadata || {},
+      methodDescriptor_Greeter_SayHello);
+};
+
+
+/**
+ * @const
+ * @type {!grpc.web.MethodDescriptor<
+ *   !proto.greet.HelloRequest,
+ *   !proto.greet.HelloReply>}
+ */
+const methodDescriptor_Greeter_SayHellos = new grpc.web.MethodDescriptor(
+  '/greet.Greeter/SayHellos',
+  grpc.web.MethodType.SERVER_STREAMING,
+  proto.greet.HelloRequest,
+  proto.greet.HelloReply,
+  /**
+   * @param {!proto.greet.HelloRequest} request
+   * @return {!Uint8Array}
+   */
+  function(request) {
+    return request.serializeBinary();
+  },
+  proto.greet.HelloReply.deserializeBinary
+);
+
+
+/**
+ * @const
+ * @type {!grpc.web.AbstractClientBase.MethodInfo<
+ *   !proto.greet.HelloRequest,
+ *   !proto.greet.HelloReply>}
+ */
+const methodInfo_Greeter_SayHellos = new grpc.web.AbstractClientBase.MethodInfo(
+  proto.greet.HelloReply,
+  /**
+   * @param {!proto.greet.HelloRequest} request
+   * @return {!Uint8Array}
+   */
+  function(request) {
+    return request.serializeBinary();
+  },
+  proto.greet.HelloReply.deserializeBinary
+);
+
+
+/**
+ * @param {!proto.greet.HelloRequest} request The request proto
+ * @param {?Object<string, string>} metadata User defined
+ *     call metadata
+ * @return {!grpc.web.ClientReadableStream<!proto.greet.HelloReply>}
+ *     The XHR Node Readable Stream
+ */
+proto.greet.GreeterClient.prototype.sayHellos =
+    function(request, metadata) {
+  return this.client_.serverStreaming(this.hostname_ +
+      '/greet.Greeter/SayHellos',
+      request,
+      metadata || {},
+      methodDescriptor_Greeter_SayHellos);
+};
+
+
+/**
+ * @param {!proto.greet.HelloRequest} request The request proto
+ * @param {?Object<string, string>} metadata User defined
+ *     call metadata
+ * @return {!grpc.web.ClientReadableStream<!proto.greet.HelloReply>}
+ *     The XHR Node Readable Stream
+ */
+proto.greet.GreeterPromiseClient.prototype.sayHellos =
+    function(request, metadata) {
+  return this.client_.serverStreaming(this.hostname_ +
+      '/greet.Greeter/SayHellos',
+      request,
+      metadata || {},
+      methodDescriptor_Greeter_SayHellos);
+};
+
+
+module.exports = proto.greet;
+
---
+++ examples/Browser/Server/wwwroot/Scripts/index.js
@@ -0,0 +1,53 @@
+﻿const { HelloRequest, HelloReply } = require('./greet_pb.js');
+const { GreeterClient } = require('./greet_grpc_web_pb.js');
+
+var client = new GreeterClient(window.location.origin);
+
+var nameInput = document.getElementById('name');
+var sendInput = document.getElementById('send');
+var streamInput = document.getElementById('stream');
+var resultText = document.getElementById('result');
+var streamingCall = null;
+
+// Unary call
+sendInput.onclick =function () {
+    var request = new HelloRequest();
+    request.setName(nameInput.value);
+
+    client.sayHello(request, {}, (err, response) => {
+        resultText.innerHTML = htmlEscape(response.getMessage());
+    });
+};
+
+// Server streaming call
+streamInput.onclick = function () {
+    if (!streamingCall) {
+        sendInput.disabled = true;
+        streamInput.value = 'Stop server stream';
+        resultText.innerHTML = '';
+
+        var request = new HelloRequest();
+        request.setName(nameInput.value);
+
+        streamingCall = client.sayHellos(request, {});
+        streamingCall.on('data', function (response) {
+            resultText.innerHTML += htmlEscape(response.getMessage()) + '<br />';
+        });
+        streamingCall.on('end', function () {
+        });
+    } else {
+        streamingCall.cancel();
+        streamingCall = null;
+        sendInput.disabled = false;
+        streamInput.value = 'Start server stream';
+    }
+};
+
+function htmlEscape(str) {
+    return String(str)
+        .replace(/&/g, '&amp;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#39;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;');
+}
