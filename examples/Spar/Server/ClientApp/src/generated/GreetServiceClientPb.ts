ae660e87fc22373c90d5b67053eb2582e0ceae7d
hanabi1224 <harlowmoo@gmail.com>
2020-02-15 20:19:46 +0800

    Add CORS example

---
+++ examples/Spar/Server/ClientApp/src/generated/GreetServiceClientPb.ts
@@ -0,0 +1,77 @@
+/**
+ * @fileoverview gRPC-Web generated client stub for greet
+ * @enhanceable
+ * @public
+ */
+
+// GENERATED CODE -- DO NOT EDIT!
+
+
+import * as grpcWeb from 'grpc-web';
+
+import {
+  HelloReply,
+  HelloRequest} from './greet_pb';
+
+export class GreeterClient {
+  client_: grpcWeb.AbstractClientBase;
+  hostname_: string;
+  credentials_: null | { [index: string]: string; };
+  options_: null | { [index: string]: string; };
+
+  constructor (hostname: string,
+               credentials?: null | { [index: string]: string; },
+               options?: null | { [index: string]: string; }) {
+    if (!options) options = {};
+    if (!credentials) credentials = {};
+    options['format'] = 'text';
+
+    this.client_ = new grpcWeb.GrpcWebClientBase(options);
+    this.hostname_ = hostname;
+    this.credentials_ = credentials;
+    this.options_ = options;
+  }
+
+  methodInfoSayHello = new grpcWeb.AbstractClientBase.MethodInfo(
+    HelloReply,
+    (request: HelloRequest) => {
+      return request.serializeBinary();
+    },
+    HelloReply.deserializeBinary
+  );
+
+  sayHello(
+    request: HelloRequest,
+    metadata: grpcWeb.Metadata | null,
+    callback: (err: grpcWeb.Error,
+               response: HelloReply) => void) {
+    return this.client_.rpcCall(
+      this.hostname_ +
+        '/greet.Greeter/SayHello',
+      request,
+      metadata || {},
+      this.methodInfoSayHello,
+      callback);
+  }
+
+  methodInfoSayHellos = new grpcWeb.AbstractClientBase.MethodInfo(
+    HelloReply,
+    (request: HelloRequest) => {
+      return request.serializeBinary();
+    },
+    HelloReply.deserializeBinary
+  );
+
+  sayHellos(
+    request: HelloRequest,
+    metadata?: grpcWeb.Metadata) {
+    return this.client_.serverStreaming(
+      this.hostname_ +
+        '/greet.Greeter/SayHellos',
+      request,
+      metadata || {},
+      this.methodInfoSayHellos);
+  }
+
+}
+
---
+++ examples/Spar/Server/Services/GreeterService.cs
@@ -0,0 +1,44 @@
+﻿#region Copyright notice and license
+
+// Copyright 2019 The gRPC Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#endregion
+
+using System;
+using System.Threading.Tasks;
+using Greet;
+using Grpc.Core;
+
+namespace Server.Services
+{
+    public class GreeterService : Greeter.GreeterBase
+    {
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply { Message = $"Hello {request.Name}" });
+        }
+
+        public override async Task SayHellos(HelloRequest request, IServerStreamWriter<HelloReply> responseStream, ServerCallContext context)
+        {
+            var i = 0;
+            while (!context.CancellationToken.IsCancellationRequested)
+            {
+                await responseStream.WriteAsync(new HelloReply { Message = $"Hello {request.Name} {i}" });
+                await Task.Delay(TimeSpan.FromSeconds(1));
+                i++;
+            }
+        }
+    }
+}
