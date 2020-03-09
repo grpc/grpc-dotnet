06fa6c2fc7933b4b30a66b4ca56995913c937d04
James Newton-King <james@newtonking.com>
2020-03-10 10:10:26 +1300

    Fix GrpcWebHandler not always resetting HTTP version back to 2.0 (#810)

--- examples/Blazor/Server/Program.cs
+++ examples/Blazor/Server/Program.cs
@@ -16,9 +16,8 @@
 
 #endregion
 
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
 namespace Server
 {
@@ -26,15 +25,14 @@ namespace Server
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseConfiguration(new ConfigurationBuilder()
-                    .AddCommandLine(args)
-                    .Build())
-                .UseStartup<Startup>()
-                .Build();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
