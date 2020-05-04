38ea7e84413ac1b9c204a1492b02c20803aa69e8
James Newton-King <james@newtonking.com>
2020-05-05 07:47:11 +1200

    Build non-preview gRPC-Web packages (#888)

--- kokoro/build_nuget.sh
+++ kokoro/build_nuget.sh
@@ -29,21 +29,13 @@ mkdir -p artifacts
 
 build/expand_dev_version.sh
 
-# "-" in the version number means prerelease
-grep -o '<GrpcDotnetVersion>.*-.*</GrpcDotnetVersion>' build/version.props && BUILDING_PREVIEW_VERSION=true
-
 (cd src/Grpc.Net.Common && dotnet pack --configuration Release --output ../../artifacts)
 (cd src/Grpc.Net.Client && dotnet pack --configuration Release --output ../../artifacts)
 (cd src/Grpc.Net.ClientFactory && dotnet pack --configuration Release --output ../../artifacts)
+(cd src/Grpc.Net.Client.Web && dotnet pack --configuration Release --output ../../artifacts)
 (cd src/Grpc.AspNetCore.Server && dotnet pack --configuration Release --output ../../artifacts)
 (cd src/Grpc.AspNetCore.Server.ClientFactory && dotnet pack --configuration Release --output ../../artifacts)
 (cd src/Grpc.AspNetCore.Server.Reflection && dotnet pack --configuration Release --output ../../artifacts)
 (cd src/Grpc.AspNetCore && dotnet pack --configuration Release --output ../../artifacts)
+(cd src/Grpc.AspNetCore.Web && dotnet pack --configuration Release --output ../../artifacts)
 (cd src/dotnet-grpc && dotnet pack --configuration Release --output ../../artifacts)
-
-if [ "${BUILDING_PREVIEW_VERSION}" == "true" ]
-then
-  # these packages are only built as pre-release
-  (cd src/Grpc.Net.Client.Web && dotnet pack --configuration Release --output ../../artifacts)
-  (cd src/Grpc.AspNetCore.Web && dotnet pack --configuration Release --output ../../artifacts)
-fi
