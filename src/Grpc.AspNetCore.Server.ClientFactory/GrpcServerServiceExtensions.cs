d8896258d984ecc14d517ac9479b5b8a0493dfc5
Bastian Eicher <bastian@eicher.net>
2020-05-09 19:20:48 +0200

    Add AddGrpcCallContextPropagation extension method
    This is an alternative to EnableCallContextPropagation that applies to call gRPC clients instead of a specific one.

---
+++ src/Grpc.AspNetCore.Server.ClientFactory/GrpcServerServiceExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Grpc.AspNetCore.ClientFactory;
+using Grpc.Core;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Grpc.AspNetCore.Server.ClientFactory
+{
+    /// <summary>
+    /// Extension methods for <see cref="IServiceCollection"/>.
+    /// </summary>
+    public static class GrpcServerServiceExtensions
+    {
+        /// <summary>
+        /// Configures the server to propagate values from a call's <see cref="ServerCallContext"/>
+        /// onto all gRPC clients.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/>.</param>
+        /// <returns>The service collection.</returns>
+        public static IServiceCollection AddGrpcCallContextPropagation(this IServiceCollection services)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            services.TryAddSingleton<ContextPropagationInterceptor>();
+            services.AddHttpContextAccessor();
+            services.ConfigureAllGrpcClients((services, options) =>
+            {
+                options.Interceptors.Add(services.GetRequiredService<ContextPropagationInterceptor>());
+            });
+
+            return services;
+        }
+    }
+}
