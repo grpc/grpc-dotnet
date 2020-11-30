cd2e76860c0be7bb7570517407ace269ae4b6e4f
James Newton-King <james@newtonking.com>
2020-12-01 11:10:20 +1300

    Update Blazor sample to use navigation manager by default (#1129)

--- examples/Blazor/Client/Program.cs
+++ examples/Blazor/Client/Program.cs
@@ -20,6 +20,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Grpc.Net.Client;
 using Grpc.Net.Client.Web;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -38,6 +39,13 @@ namespace Client
                 // Get the service address from appsettings.json
                 var config = services.GetRequiredService<IConfiguration>();
                 var backendUrl = config["BackendUrl"];
+
+                // If no address is set then fallback to the current webpage URL
+                if (string.IsNullOrEmpty(backendUrl))
+                {
+                    var navigationManager = services.GetRequiredService<NavigationManager>();
+                    backendUrl = navigationManager.BaseUri;
+                }
 
                 // Create a channel with a GrpcWebHandler that is addressed to the backend server.
                 //
