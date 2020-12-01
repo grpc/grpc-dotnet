bded9f9b51b249a022ee959bf4fdbf2cfe30f397
James Newton-King <james@newtonking.com>
2020-12-01 13:40:44 +1300

    Improve streaming error messages when call is complete (#1121)

--- test/FunctionalTests/Server/DeadlineTests.cs
+++ test/FunctionalTests/Server/DeadlineTests.cs
@@ -341,7 +341,7 @@ namespace Grpc.AspNetCore.FunctionalTests.Server
                 var errorLogged = Logs.Any(r =>
                     r.EventId.Name == "ErrorExecutingServiceMethod" &&
                     r.State.ToString() == "Error when executing service method 'WriteUntilError'." &&
-                    (r.Exception!.Message == "Cannot write message after request is complete." || r.Exception!.Message == "Writing is not allowed after writer was completed."));
+                    (r.Exception!.Message == "Can't write the message because the request is complete." || r.Exception!.Message == "Writing is not allowed after writer was completed."));
 
                 return errorLogged;
             }, "Expected error not thrown.").DefaultTimeout();
