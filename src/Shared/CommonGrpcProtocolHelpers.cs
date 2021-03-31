81425392bf0bc8c4ab71a0494f12bb80f522094c
James Newton-King <james@newtonking.com>
2021-03-31 21:41:24 +1300

    Fix

--- src/Shared/CommonGrpcProtocolHelpers.cs
+++ src/Shared/CommonGrpcProtocolHelpers.cs
@@ -24,6 +24,9 @@ namespace Grpc.Shared
 {
     internal static class CommonGrpcProtocolHelpers
     {
+        public static readonly Task<bool> TrueTask = Task.FromResult(true);
+        public static readonly Task<bool> FalseTask = Task.FromResult(false);
+
         // Timer and DateTime.UtcNow have a 14ms precision. Add a small delay when scheduling deadline
         // timer that tests if exceeded or not. This avoids rescheduling the deadline callback multiple
         // times when timer is triggered before DateTime.UtcNow reports the deadline has been exceeded.
@@ -116,8 +119,5 @@ namespace Grpc.Shared
 
             return sb.ToString();
         }
-
-        public static readonly Task<bool> TrueTask = Task.FromResult(false);
-        public static readonly Task<bool> FalseTask = Task.FromResult(false);
     }
 }
