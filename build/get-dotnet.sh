785578dc438654ebfbd44bd3db6197cf6947d807
James Newton-King <james@newtonking.com>
2021-08-04 13:00:24 +1200

    Fix?

--- build/get-dotnet.sh
+++ build/get-dotnet.sh
@@ -18,7 +18,10 @@ ensure_dir() {
 # main
 
 # resolve SDK version
-sdk_version=$(jq -r .sdk.version $global_json_path)
+#sdk_version=$(jq -r .sdk.version $global_json_path)
+
+# TODO(JamesNK): Temporarily override global.json while using preview version
+sdk_version="6.0.100-rc.1.21380.19"
 
 # download dotnet-install.sh
 ensure_dir $OBJDIR
