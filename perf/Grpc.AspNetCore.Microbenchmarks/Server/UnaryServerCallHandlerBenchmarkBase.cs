14639cd3d0d6c3fc814a91179c99d55592ea06d1
James Newton-King <james@newtonking.com>
2022-01-21 07:42:15 +1300

    Clean up

--- perf/Grpc.AspNetCore.Microbenchmarks/Server/UnaryServerCallHandlerBenchmarkBase.cs
+++ perf/Grpc.AspNetCore.Microbenchmarks/Server/UnaryServerCallHandlerBenchmarkBase.cs
@@ -29,7 +29,6 @@ using Grpc.Core;
 using Grpc.Net.Compression;
 using Grpc.Shared.Server;
 using Grpc.Tests.Shared;
-using Microsoft.AspNetCore.Connections.Features;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.Extensions.DependencyInjection;
