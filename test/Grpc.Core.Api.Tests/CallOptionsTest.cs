819dea5af6dd4a4591715371682a8ae36882d8f3
Jan Tattermusch <jtattermusch@google.com>
2022-04-28 10:22:46 +0200

    Move Grpc.Core.Api nuget from grpc/grpc (#1651)
    * add Grpc.Core.Api tree (at SHA dd14f803c3beb433c41be91672c6a7a6eeb8727c)
    
    https://github.com/grpc/grpc/tree/dd14f803c3beb433c41be91672c6a7a6eeb8727c/src/csharp/Grpc.Core.Api
    
    * initial adjustments to make Grpc.Core.Api buildable
    
    * fix analyzer warnings in Grpc.Core.Api
    
    * net462 fixup
    
    * bin obj already in gitignore
    
    * add TODOs
    
    * add Grpc.Core.Api to the solution
    
    * pack README.md
    
    * start using net45 again
    
    * add Grpc.Core.Tests tree at e9ece8f7469e667d5202be134bd88980630dba95
    
    https://github.com/grpc/grpc/tree/e9ece8f7469e667d5202be134bd88980630dba95/src/csharp/Grpc.Core.Tests
    
    * remove gitignore
    
    * initial adjustments
    
    * more project adjustments
    
    * remove Grpc.Core specific tests
    
    * Grpc.Core.Api: make internals visible to Grpc.Core.Api.Tests
    
    * remove ChannelOptionsTest (only relevant to Grpc.Core
    
    * CallOptions.Normalize is only implemented in Grpc.Core
    
    * CallCredential.ToNativeCredentials extension method only exists in Grpc.Core
    
    * CallOptionsTest: add TODO
    
    * ChannelCredentials.ToNativeCredentials() only exists in Grpc.Core
    
    * add Grpc.Core.Api.Tests to the solution
    
    * cleanup Grpc.Core.Api.Tests.csproj
    
    * fix versions in VersionInfo.cs
    
    * Version.cs no longer needed (same attributes set elsewhere
    
    * add VersionInfoTest to make sure VersionInfo.cs stays up to date
    
    * cleanup Grpc.Core.Api.csproj
    
    * remove net45-compatibility code
    
    * switch Grpc.Core.Api to net462
    
    * use ProjectReference for Grpc.Core.Api
    
    * add script for updating VersionInfo.cs
    
    * update VersionInfo.cs

---
+++ test/Grpc.Core.Api.Tests/CallOptionsTest.cs
@@ -0,0 +1,80 @@
+#region Copyright notice and license
+
+// Copyright 2015 gRPC authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using Grpc.Core;
+using Grpc.Core.Internal;
+using Grpc.Core.Utils;
+using NUnit.Framework;
+
+namespace Grpc.Core.Tests
+{
+    public class CallOptionsTest
+    {
+        [Test]
+        public void WithMethods()
+        {
+            var options = new CallOptions();
+            
+            var metadata = new Metadata();
+            Assert.AreSame(metadata, options.WithHeaders(metadata).Headers);
+
+            var deadline = DateTime.UtcNow;
+            Assert.AreEqual(deadline, options.WithDeadline(deadline).Deadline.Value);
+
+            var cancellationToken = new CancellationTokenSource().Token;
+            Assert.AreEqual(cancellationToken, options.WithCancellationToken(cancellationToken).CancellationToken);
+
+            var writeOptions = new WriteOptions();
+            Assert.AreSame(writeOptions, options.WithWriteOptions(writeOptions).WriteOptions);
+
+            // TODO(jtattermusch): add back test for .WithPropagationToken() method
+            // var propagationToken = new ContextPropagationTokenImpl(CallSafeHandle.NullInstance, DateTime.UtcNow,
+            //     CancellationToken.None, ContextPropagationOptions.Default);
+            // Assert.AreSame(propagationToken, options.WithPropagationToken(propagationToken).PropagationToken);
+
+            var credentials = new FakeCallCredentials();
+            Assert.AreSame(credentials, options.WithCredentials(credentials).Credentials);
+
+            var flags = CallFlags.WaitForReady | CallFlags.CacheableRequest;
+            Assert.AreEqual(flags, options.WithFlags(flags).Flags);
+
+            // Check that the original instance is unchanged.
+            Assert.IsNull(options.Headers);
+            Assert.IsNull(options.Deadline);
+            Assert.AreEqual(CancellationToken.None, options.CancellationToken);
+            Assert.IsNull(options.WriteOptions);
+            Assert.IsNull(options.PropagationToken);
+            Assert.IsNull(options.Credentials);
+            Assert.AreEqual(default(CallFlags), options.Flags);
+        }
+
+        [Test]
+        public void WaitForReady()
+        {
+            var callOptions = new CallOptions();
+            Assert.IsFalse(callOptions.IsWaitForReady);
+
+            Assert.AreEqual(CallFlags.WaitForReady, callOptions.WithWaitForReady().Flags);
+            Assert.IsTrue(callOptions.WithWaitForReady().IsWaitForReady);
+            Assert.IsFalse(callOptions.WithWaitForReady(true).WithWaitForReady(false).IsWaitForReady);
+        }
+    }
+}
