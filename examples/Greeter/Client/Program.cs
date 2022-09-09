1361655c4892f0dc53acbfb3e80753b74850a19c
James Newton-King <james@newtonking.com>
2022-09-09 10:35:05 +0800

    More

--- examples/Greeter/Client/Program.cs
+++ examples/Greeter/Client/Program.cs
@@ -16,26 +16,15 @@
 
 #endregion
 
-using System;
-using System.Threading.Tasks;
 using Greet;
 using Grpc.Net.Client;
 
-namespace Client
-{
-    public class Program
-    {
-        static async Task Main(string[] args)
-        {
-            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
-            var client = new Greeter.GreeterClient(channel);
+using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+var client = new Greeter.GreeterClient(channel);
 
-            var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
-            Console.WriteLine("Greeting: " + reply.Message);
+var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
+Console.WriteLine("Greeting: " + reply.Message);
 
-            Console.WriteLine("Shutting down");
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadKey();
-        }
-    }
-}
+Console.WriteLine("Shutting down");
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
