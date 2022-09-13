3e12474a633f0bdf5b3ac2e722728ab8125f8503
James Newton-King <james@newtonking.com>
2022-09-13 10:29:46 +0800

    Change examples to use WebApplication V1 (#1875)

--- examples/Aggregator/Client/Program.cs
+++ examples/Aggregator/Client/Program.cs
@@ -16,68 +16,54 @@
 
 #endregion
 
-using System;
-using System.Threading;
-using System.Threading.Tasks;
 using Aggregate;
 using Count;
 using Greet;
 using Grpc.Core;
 using Grpc.Net.Client;
 
-namespace Client
+using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+var client = new Aggregator.AggregatorClient(channel);
+
+await ServerStreamingCallExample(client);
+
+await ClientStreamingCallExample(client);
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
+
+static async Task ServerStreamingCallExample(Aggregator.AggregatorClient client)
 {
-    public class Program
+    var cts = new CancellationTokenSource();
+    cts.CancelAfter(TimeSpan.FromSeconds(3.5));
+
+    using var call = client.SayHellos(new HelloRequest { Name = "AggregatorClient" }, cancellationToken: cts.Token);
+    try
     {
-        private static readonly Random Random = new Random();
-
-        static async Task Main(string[] args)
+        await foreach (var message in call.ResponseStream.ReadAllAsync())
         {
-            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
-            var client = new Aggregator.AggregatorClient(channel);
-
-            await ServerStreamingCallExample(client);
-
-            await ClientStreamingCallExample(client);
-
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadKey();
-        }
-
-        private static async Task ServerStreamingCallExample(Aggregator.AggregatorClient client)
-        {
-            var cts = new CancellationTokenSource();
-            cts.CancelAfter(TimeSpan.FromSeconds(3.5));
-
-            using var call = client.SayHellos(new HelloRequest { Name = "AggregatorClient" }, cancellationToken: cts.Token);
-            try
-            {
-                await foreach (var message in call.ResponseStream.ReadAllAsync())
-                {
-                    Console.WriteLine("Greeting: " + message.Message);
-                }
-            }
-            catch (RpcException ex) when (ex.StatusCode == StatusCode.Cancelled)
-            {
-                Console.WriteLine("Stream cancelled.");
-            }
-        }
-
-        private static async Task ClientStreamingCallExample(Aggregator.AggregatorClient client)
-        {
-            using var call = client.AccumulateCount();
-            for (var i = 0; i < 3; i++)
-            {
-                var count = Random.Next(5);
-                Console.WriteLine($"Accumulating with {count}");
-                await call.RequestStream.WriteAsync(new CounterRequest { Count = count });
-                await Task.Delay(2000);
-            }
-
-            await call.RequestStream.CompleteAsync();
-
-            var response = await call;
-            Console.WriteLine($"Count: {response.Count}");
+            Console.WriteLine("Greeting: " + message.Message);
         }
     }
+    catch (RpcException ex) when (ex.StatusCode == StatusCode.Cancelled)
+    {
+        Console.WriteLine("Stream cancelled.");
+    }
+}
+
+static async Task ClientStreamingCallExample(Aggregator.AggregatorClient client)
+{
+    using var call = client.AccumulateCount();
+    for (var i = 0; i < 3; i++)
+    {
+        var count = Random.Shared.Next(5);
+        Console.WriteLine($"Accumulating with {count}");
+        await call.RequestStream.WriteAsync(new CounterRequest { Count = count });
+        await Task.Delay(2000);
+    }
+
+    await call.RequestStream.CompleteAsync();
+
+    var response = await call;
+    Console.WriteLine($"Count: {response.Count}");
 }
