347dfead5735d5bc0d54e33409d5426fbdf2042c
Jan Tattermusch <jtattermusch@google.com>
2023-03-03 11:19:38 +0100

    Upgrade version to 2.52.0-pre1 (on v2.52.x branch) (#2066)
    * Update version.props
    
    * Update VersionInfo.cs

--- src/Grpc.Core.Api/VersionInfo.cs
+++ src/Grpc.Core.Api/VersionInfo.cs
@@ -41,5 +41,5 @@ public static class VersionInfo
     /// <summary>
     /// Current version of gRPC C#
     /// </summary>
-    public const string CurrentVersion = "2.52.0-dev";
+    public const string CurrentVersion = "2.52.0-pre1";
 }
