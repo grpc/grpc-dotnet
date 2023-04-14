29661a49c5c25af2665d66440e3d6dc49644b82e
James Newton-King <james@newtonking.com>
2023-04-14 15:16:42 +0800

    Fix jest tests?

--- testassets/InteropTestsGrpcWebWebsite/Tests/custom-environment.js
+++ testassets/InteropTestsGrpcWebWebsite/Tests/custom-environment.js
@@ -3,7 +3,7 @@
 function _interopDefault(ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
 
 const JestPuppeteerEnvironment = require("jest-environment-puppeteer").TestEnvironment;
-const expect = _interopDefault(require('expect-puppeteer'));
+const expect = require('expect-puppeteer').expect;
 
 class CustomEnvironment extends JestPuppeteerEnvironment {
     // Load page and get test names to run
@@ -16,7 +16,7 @@ class CustomEnvironment extends JestPuppeteerEnvironment {
         await page.goto('http:localhost:8081', { waitUntil: 'networkidle0' });
 
         // Wait for Blazor to finish loading
-        await expect(page).toMatch('gRPC-Web interop tests');
+        await expect(page).toMatchTextContent('gRPC-Web interop tests');
 
         // Get test names
         this.global.__GRPC_WEB_TEST_NAMES__ = await page.evaluate(() => getTestNames('GrpcWeb'));
