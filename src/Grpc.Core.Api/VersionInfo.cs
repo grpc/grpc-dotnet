a90b3de004bc76553afaa40dacbf150a28ef4840
Alexander Polcyn <apolcyn@google.com>
2023-10-06 23:45:44 +0000

    bump to pre1 on 2.58 branch

--- src/Grpc.Core.Api/VersionInfo.cs
+++ src/Grpc.Core.Api/VersionInfo.cs
@@ -41,5 +41,5 @@ public static class VersionInfo
     /// <summary>
     /// Current version of gRPC C#
     /// </summary>
-    public const string CurrentVersion = "2.58.0-dev";
+    public const string CurrentVersion = "2.58.0-pre1";
 }
