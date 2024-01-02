b84d77267384e480324f2cd4ed5f4a7ff879f2c6
James Newton-King <james@newtonking.com>
2024-01-02 14:14:07 +0800

    Fix cancellation token reported when using retries

--- src/Grpc.Net.Client/Internal/Retry/RetryCallBase.cs
+++ src/Grpc.Net.Client/Internal/Retry/RetryCallBase.cs
@@ -529,7 +529,7 @@ internal abstract partial class RetryCallBase<TRequest, TResponse> : IGrpcCall<T
         CommitReason commitReason;
 
         // Cancellation token triggered by dispose could throw here.
-        if (ex is OperationCanceledException && CancellationTokenSource.IsCancellationRequested)
+        if (ex is OperationCanceledException operationCanceledException && CancellationTokenSource.IsCancellationRequested)
         {
             // Cancellation could have been caused by an exceeded deadline.
             if (IsDeadlineExceeded())
@@ -542,7 +542,20 @@ internal abstract partial class RetryCallBase<TRequest, TResponse> : IGrpcCall<T
             else
             {
                 commitReason = CommitReason.Canceled;
-                resolvedCall = CreateStatusCall(Disposed ? GrpcProtocolConstants.CreateDisposeCanceledStatus(ex) : GrpcProtocolConstants.CreateClientCanceledStatus(ex));
+                Status status;
+                if (Disposed)
+                {
+                    status = GrpcProtocolConstants.CreateDisposeCanceledStatus(exception: null);
+                }
+                else
+                {
+                    if (Options.CancellationToken.IsCancellationRequested && Options.CancellationToken != operationCanceledException.CancellationToken)
+                    {
+                        ex = new OperationCanceledException(Options.CancellationToken);
+                    }
+                    status = GrpcProtocolConstants.CreateClientCanceledStatus(ex);
+                }
+                resolvedCall = CreateStatusCall(status);
             }
         }
         else
