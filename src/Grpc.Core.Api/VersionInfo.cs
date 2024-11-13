e0182fe8f7a278948a4767bf9cd4fb48c67b140c
Alexander Polcyn <apolcyn@google.com>
2024-11-13 03:50:35 +0000

    bump to final version

--- src/Grpc.Core.Api/VersionInfo.cs
+++ src/Grpc.Core.Api/VersionInfo.cs
@@ -41,5 +41,5 @@ public static class VersionInfo
     /// <summary>
     /// Current version of gRPC C#
     /// </summary>
-    public const string CurrentVersion = "2.67.0-pre1";
+    public const string CurrentVersion = "2.67.0";
 }
