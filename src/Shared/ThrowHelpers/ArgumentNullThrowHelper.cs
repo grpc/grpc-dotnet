3bf19a113cbdb09012c14cc253f146ec822de11c
Weihan Li <weihanli@outlook.com>
2024-12-09 08:36:19 +0800

    use nameof for CallerArgumentExpression (#2562)

--- src/Shared/ThrowHelpers/ArgumentNullThrowHelper.cs
+++ src/Shared/ThrowHelpers/ArgumentNullThrowHelper.cs
@@ -11,7 +11,7 @@ internal static partial class ArgumentNullThrowHelper
     /// <summary>Throws an <see cref="ArgumentNullException"/> if <paramref name="argument"/> is null.</summary>
     /// <param name="argument">The reference type argument to validate as non-null.</param>
     /// <param name="paramName">The name of the parameter with which <paramref name="argument"/> corresponds.</param>
-    public static void ThrowIfNull([NotNull] object? argument, [CallerArgumentExpression("argument")] string? paramName = null)
+    public static void ThrowIfNull([NotNull] object? argument, [CallerArgumentExpression(nameof(argument))] string? paramName = null)
     {
 #if !NET6_0_OR_GREATER
         if (argument is null)
